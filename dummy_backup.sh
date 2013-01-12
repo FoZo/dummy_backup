755dfa730076063b3ca15fbe190585dda13823e3
FoZo <georgy.vas@gmail.com>
2013-01-12 19:13:57 +0200

    Update dummy_backup.sh

--- dummy_backup.sh
+++ dummy_backup.sh
@@ -3,94 +3,99 @@
 # Configs
 DATE=$(date +%_d_%m_%Y)
 
-DIRS="bin home  lib32 run usr boot etc lib lib64 opt root sbin var"
-EXCLUDE_DIRS="/var/tmp/* /usr/portage/*"
+DIRS="home bin lib32 run usr boot etc lib lib64 opt root sbin var"
+EXCLUDE_DIRS="/home/walker/Downloads/* /var/tmp/* /usr/portage/* /var/log/*"
 
-SERVER_NAME="latop"
-PASSPHRASE="$DATE"
-BACKUP_DIR="/mnt/Backup/backup-$SERVER_NAME/$DATE"
+SERVER_NAME="acer_latop" # Set name for backup dir
+NFS_SERVER_IP="set NFS server"
+PASSPHRASE="test-$DATE" # Set passphrase for gpg encryption
+BACKUP_DIR="/mnt/Backup/backup-$SERVER_NAME/$DATE" # Set path to backup dir
+
+
 
 function check_user {
 
-	if [ "$(whoami)" != "root" ] ; then
-		echo "Only root can run this backup script"
-		exit
-	fi
+        if [ "$(whoami)" != "root" ] ; then
+                echo "Only root can run this backup script"
+                exit
+        fi
 }
 
 function mount_nfs {
-	
-# 1. Mount Bakup
-	if mount|grep '/volume1/NetBackup' 2>&1 ; then
-                echo "Mount exist, skipping this step"
-	elif ping -c 1 192.168.1.125 -q 2>&1 ; then
-		echo "Mounting nfs backup"
-		if ! mount.nfs 192.168.1.125:/volume1/NetBackup /mnt/Backup/ -o nolock ; then
-			echo "Can't mount nfs"
-			exit
-		fi
-	else
-		echo "Can't mount"
-		exit 0
-	fi
+
+        echo "Mount NFS"
+        if mount|grep '/volume1/NetBackup' 2>&1 ; then
+                echo " Mount exist, skipping this step"
+        elif ping -c 1 $NFS_SERVER_IP -q 2>&1 ; then
+                echo "Mounting nfs backup"
+                if ! mount.nfs $NFS_SERVER_IP:/volume1/NetBackup /mnt/Backup/ -o nolock 2>&1 ; then
+                        echo " Can't mount nfs"
+                        exit
+                fi
+        else
+                echo " Can't mount"
+                exit 0
+        fi
 }
 
 function mkback_dir {
-	
-# 2. Making Backup dir
-	if [ -d $BACKUP_DIR ] ; then
-		echo "Backup dir exist"
-	else
-		echo "Making $BACKUP_DIR"
-		mkdir -p $BACKUP_DIR
-	fi
+
+        echo "Making Backup dir"
+        if [ -d $BACKUP_DIR ] ; then
+                echo " Backup dir exist"
+        else
+                echo " Making $BACKUP_DIR"
+                mkdir -p $BACKUP_DIR
+        fi
 
 }
 
 function check_backup_type {
 
-	if [ $(date +%u) != "7" ] ; then
-		echo "Making incremental backup"
-		BACKUP_T="i"
-	else
-		echo "Backing full backup"
-		BACKUP_T="f"
-	fi 
+        if [ $(date +%u) != "7" ] ; then
+                echo "Making incremental backup"
+                BACKUP_T="i"
+        else
+                echo "Backing full backup"
+                BACKUP_T="f"
+        fi 
 
 }
 
 function mk_backup {
-	
-# 3. Bakup dirs
 
-	EXCLUDE=""	
+        echo "Create backup files"
 
-	for LIST in $EXCLUDE_DIRS
-	do 
-		EXCLUDE=$EXCLUDE" --exclude=$LIST"
-	done
+        EXCLUDE=""
 
-	for DIR in $DIRS
-	do
-		if [ $BACKUP_T != "i" ] ; then
-			if [ ! -f $BACKUP_DIR/$DIR.`date +%_d-%m-%Y`.f.ok ] ; then
-				tar czvf - /$DIR $EXCLUDE 2>&1 | gpg2 -c --batch --yes --passphrase t -o $BACKUP_DIR/$DIR.`date +%_d-%m-%Y`.f.tar.gz.gpg
-				touch /mnt/Backup/$DIR.`date +%_d-%m-%Y`.f.ok
-			fi
-		else
-			if [ ! -f $BACKUP_DIR/$DIR.`date +%_d-%m-%Y`.i.ok ] ; then
-				tar czvf - /$DIR --newer-mtime='1' $EXCLUDE 2>&1 | gpg2 -c --batch --yes --passphrase t -o $BACKUP_DIR/$DIR.`date +%_d-%m-%Y`.i.tar.gz.gpg
-                        	touch $BACKUP_DIR/$DIR.`date +%_d-%m-%Y`.i.ok
-			fi
-		fi
-	done
+        for LIST in $EXCLUDE_DIRS
+        do 
+                EXCLUDE=$EXCLUDE" --exclude=$LIST"
+        done
+
+        for DIR in $DIRS
+        do
+                if [ $BACKUP_T != "i" ] ; then
+                        if [ ! -f $BACKUP_DIR/$DIR.`date +%_d-%m-%Y`.f.ok ] ; then
+                                tar czvf - /$DIR $EXCLUDE 2>&1 | gpg2 -c --batch --yes --passphrase t -o $BACKUP_DIR/$DIR.`date +%_d-%m-%Y`.f.tar.gz.gpg
+                                touch /mnt/Backup/$DIR.`date +%_d-%m-%Y`.f.ok
+                        fi
+                else
+                        if [ ! -f $BACKUP_DIR/$DIR.`date +%_d-%m-%Y`.i.ok ] ; then
+                                tar czvf - /$DIR --newer-mtime='1' $EXCLUDE 2>&1 | gpg2 -c --batch --yes --passphrase t -o $BACKUP_DIR/$DIR.`date +%_d-%m-%Y`.i.tar.gz.gpg
+                                touch $BACKUP_DIR/$DIR.`date +%_d-%m-%Y`.i.ok
+                        fi
+                fi
+        done
 }
 
+echo "-------------------------"
 echo "Start backuping at `date`."
 check_user
 mount_nfs
 mkback_dir
 check_backup_type
 mk_backup
-#umount /mnt/Backup/
+umount /mnt/Backup/
 echo "Finish backuping at `date`."
+echo "-------------------------"
